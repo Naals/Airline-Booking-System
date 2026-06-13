4185e39ef7da9976f2104b92480a94086fd5b79c
Aslan <tynyshtykaslan06@gmail.com>
2026-06-13 17:50:26 -0400

    feat(common-lib): response

---
+++ common-lib/src/main/java/com/project/commonlib/payload/response/CityResponse.java
@@ -0,0 +1,23 @@
+package com.project.commonlib.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CityResponse {
+
+    private Long id;
+    private String name;
+    private String cityCode;
+    private String countryCode;
+    private String country;
+    private String timeZone;
+    private String regionCode;
+}
